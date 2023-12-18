8f9704bf8c26b3d89fea078084a3f06efe4cb676
OmarBaRaean <124505787+OmarBaRaean@users.noreply.github.com>
2023-12-18 08:59:24 +0300

    BE_3-exercise_3

--- app.js
+++ app.js
@@ -8,4 +8,11 @@ app.get('/', (req,res) =>{
     res.sendFile(path.join(__dirname,"html1/exercis1.html" ))
 })
 
+app.get('/Routers/:r', (req,res) =>{
+    const {r} = req.params
+    res.send(`<h1>you have used the router ${r}</h1>`)
+})
+
+
+
 app.listen(5000)
