d87713d911b9de2703069586bff867f6fdffcc60
OmarBaRaean <124505787+OmarBaRaean@users.noreply.github.com>
2023-12-18 09:16:54 +0300

    BE_3-exercise_4

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+const { countReset, timeStamp } = require('console')
 const express = require('express')
 const path = require('path')
 const app = express()
 
 app.use(express.static("html1"))
+
+const mid= (req,res,next) =>{
+    console.log(`middle`)
+  
+    next()
+}
 
 app.get('/', (req,res) =>{
     res.sendFile(path.join(__dirname,"html1/exercis1.html" ))
@@ -11,6 +18,11 @@ app.get('/', (req,res) =>{
 app.get('/Routers/:r', (req,res) =>{
     const {r} = req.params
     res.send(`<h1>you have used the router ${r}</h1>`)
+})
+
+app.get('/middleware',mid, (req,res) =>{
+    const {r} = req.params
+    res.send(`<h1>${req.path} </h1> <h1>have use Middleware</h1>`)
 })
 
 
